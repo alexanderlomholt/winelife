3ffff060bad7a94567946910983a94dcdadf13f9
Claire Dupont <claire.dupont@kedgebs.com>
2018-03-06 11:23:02 -0500

    add active record relations and validation

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,2 +1,8 @@
 class Store < ApplicationRecord
+	has_many: :wine
+	has_many: :stock
+	has_many: :user
+
+	validates :name, presence: true, uniqueness: true
+	validates :address, presence: true, uniqueness: true 
 end
