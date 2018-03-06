700bb64026946a6361e25cdbf0e926845aa10c5f
Victor Bianchi <vbianchi90@gmail.com>
2018-03-06 14:22:37 -0500

    Update store.rb

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,7 +1,7 @@
 class Store < ApplicationRecord
-	has_many: :wine
-	has_many: :stock
-	has_many: :user
+	has_many: :wines, through: :stocks
+	has_many: :stocks
+
 
 	validates :name, presence: true, uniqueness: true
 	validates :address, presence: true, uniqueness: true 
